24d73460da473a5701465e8d12cac08f73e5ed21
Oskar Wolff <owolff@student.csdspartans.org>
2017-04-05 22:15:01 -0400

    updated .gitignore and finished event getter function

--- main.py
+++ main.py
@@ -1,41 +1,45 @@
 import todoist
 import requests
 from requests_oauthlib import OAuth1
+import datetime
+import credentials
 
 
-# a bunch of constants that I'll port into another file eventually
-user_id = '24163835'
-client_key = '3f370efb5e18b1e0f664e55bf4ab014a058d31b65'
-client_secret = '4ea251c386d021a01cd18937e357951c'
-todoist_token = '914638fdc19f19e4c1aabc7fb8d8d0158e823a66'
+class Event:
+    def __init__(self, name, date):
+        self.name = name
+        self.date = date
 
 
-# the functions that can pull data from schoology
+def get_events():
+    '''
+    returns a list of Event objects that are scheduled from today forward
+    '''
+    # create OAuth key
+    oauth = OAuth1(client_key=credentials.schoology_client_key, client_secret=credentials.schoology_client_secret, signature_type='auth_header')
+
+    # create headers for requests
+    headers = {'Accept': 'application/json',
+               'Host': 'api.schoology.com',
+               }
+
+    url = 'https://api.schoology.com/v1/users/{}/events'.format(credentials.schoology_user_id)
+    today = str(datetime.date.today())
+    parameters = {'start_date': today}
+
+    # send get request to specified URL with params and everything
+    # requests is great!
+    r = requests.get(url, headers=headers, auth=oauth, params=parameters)
+    events_list = r.json()['event']
+
+    events = []
+
+    for event in events_list:
+        event_name = event['title']
+        event_date = event['start']
+        events.append(Event(event_name, event_date))
+
+    return events
 
 
-# create OAuth key
-oauth = OAuth1(client_key=client_key, client_secret=client_secret, signature_type='auth_header')
-
-# create headers for requests
-headers = {'Accept': 'application/json',
-           'Host': 'api.schoology.com',
-           }
-
-
-def show_events():
-    url = 'https://api.schoology.com/v1/users/{}/events?start=0&limit=500'.format(user_id)
-
-    # send get request to specified URL
-    r = requests.get(url, headers=headers, auth=oauth)
-    # events = r.json()['event']
-    #
-    # for event in events:
-    #     print(event['title'], '\n   ', event['start'])
-    print(r.json())
-
-show_events()
-
-
-
-# the functions that push data to todoist
 # docs: https://developer.todoist.com/#read-resources
